80da1935470fe6a696d46a0934f95ff53e2f620b
Matthew Jeorrett <Matthew@wallet.services>
2020-07-13 15:08:56 +0100

    Add project files.

---
+++ OcelotAggregation/MyAggregator.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Ocelot.Middleware;
+using Ocelot.Multiplexer;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace OcelotAggregation
+{
+    public class MyAggregator : IDefinedAggregator
+    {
+        private readonly ILogger<MyAggregator> _logger;
+
+        public MyAggregator(ILogger<MyAggregator> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task<DownstreamResponse> Aggregate(List<HttpContext> responses)
+        {
+            var responseBodies = responses.Select(response =>
+            {
+                _logger.LogDebug($"Status code {response.Response.StatusCode}.");
+
+                using var responseReader = new StreamReader(response.Response.Body);
+                return responseReader.ReadToEnd();
+            }).ToList();
+
+            return new DownstreamResponse(
+                new StringContent(JsonConvert.SerializeObject(responseBodies)),
+                HttpStatusCode.OK,
+                new List<Header>(),
+                "OK");
+        }
+    }
+}
